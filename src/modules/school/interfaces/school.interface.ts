f8b87625a69506977a3c0751230a4118542c3593
Matheus Cassiano <matheuscassiano9@gmail.com>
2024-07-13 17:59:01 -0300

    feat: Add relations and update entities for driving classes

--- src/modules/school/interfaces/school.interface.ts
+++ src/modules/school/interfaces/school.interface.ts
@@ -1,7 +1,7 @@
 import { IBase } from 'src/config/base.interface';
 import { ICourse } from 'src/modules/course/interfaces/course.interface';
-import { User } from 'src/modules/user/entities/user.entity';
-import { Vehicle } from 'src/modules/vehicle/entities/vehicle.entity';
+import { IUser } from 'src/modules/user/interfaces/user.interface';
+import { IVehicle } from 'src/modules/vehicle/interfaces/vehicles.interface';
 
 export interface ISchool extends IBase {
   name: string;
@@ -11,6 +11,6 @@ export interface ISchool extends IBase {
   email: string;
   location: string;
   courses: ICourse[];
-  vehicles: Vehicle[];
-  users: User[];
+  vehicles: IVehicle[];
+  users: IUser[];
 }
