aa15f470f407a3cefebfe4f1b924c865a9386801
Matheus Cassiano <matheuscassiano9@gmail.com>
2024-07-13 09:03:19 -0300

    feat: add migration for creating vehicles table and foreign key to schools

---
+++ src/config/database/migrations/1720871689589-CreateTableVehicles.ts
@@ -0,0 +1,103 @@
+import { VehicleModel } from 'src/modules/vehicle/enums/vehicle-model.enum';
+import { VehicleStatus } from 'src/modules/vehicle/enums/vehicle-status.enum';
+import { VehicleType } from 'src/modules/vehicle/enums/vehicle-type.enum';
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateTableVehicles1720871689589 implements MigrationInterface {
+  private vehiclesTable = new Table({
+    name: 'vehicles',
+    columns: [
+      {
+        name: 'id',
+        type: 'int',
+        isPrimary: true,
+        isGenerated: true,
+        generationStrategy: 'increment',
+      },
+      {
+        name: 'name',
+        type: 'VARCHAR',
+        length: '255',
+      },
+      {
+        name: 'year',
+        type: 'INTERGER',
+      },
+      {
+        name: 'plate',
+        type: 'VARCHAR',
+        length: '255',
+      },
+      {
+        name: 'brand',
+        type: 'VARCHAR',
+        length: '255',
+      },
+      {
+        name: 'color',
+        type: 'VARCHAR',
+        length: '255',
+      },
+      {
+        name: 'model',
+        type: 'enum',
+        enum: Object.keys(VehicleModel),
+        default: `'${VehicleModel.SUV}'`,
+      },
+      {
+        name: 'type',
+        type: 'enum',
+        enum: Object.keys(VehicleType),
+        default: `'${VehicleType.CAR}'`,
+      },
+      {
+        name: 'status',
+        type: 'enum',
+        enum: Object.keys(VehicleStatus),
+        default: `'${VehicleStatus.AVAILABLE}'`,
+      },
+      {
+        name: 'school_id',
+        type: 'INTEGER',
+        isNullable: false,
+      },
+      {
+        name: 'created_at',
+        type: 'TIMESTAMP',
+        default: 'NOW()',
+      },
+      {
+        name: 'updated_at',
+        type: 'TIMESTAMP',
+        default: 'NOW()',
+      },
+      {
+        name: 'deleted_at',
+        type: 'TIMESTAMP',
+        isNullable: true,
+      },
+    ],
+  });
+
+  private foreignKey = new TableForeignKey({
+    columnNames: ['school_id'],
+    referencedColumnNames: ['id'],
+    referencedTableName: 'schools',
+    onDelete: 'CASCADE',
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.vehiclesTable);
+    await queryRunner.createForeignKey('vehicles', this.foreignKey);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('vehicles', this.foreignKey);
+    await queryRunner.dropTable(this.vehiclesTable);
+  }
+}
